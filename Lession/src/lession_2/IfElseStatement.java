b8c1d4b1f88864cc554548e36bbc625cafb4e39a
thinh160895 <tvthinh1608.danang@gmail.com>
2024-02-18 21:06:43 +0700

    Update src Lab location Update OperatorLearning.java Fix bug in IfElseStatement.java

--- Lession/src/lession_2/IfElseStatement.java
+++ Lession/src/lession_2/IfElseStatement.java
@@ -3,14 +3,15 @@ package lession_2;
 import java.sql.SQLOutput;
 
 public class IfElseStatement {
-    public static void  main(String[] args){
+    public static void  main(String[] args) {
         // Comparison Operators: == (same?), != (diff?), >, <, >=, <=
 
         final boolean isHeOnTime = false;
         System.out.println("1. Dat xe ra");
         System.out.println("2. Chay ra quan");
 
-        if(isHeOnTime) {
+        if (isHeOnTime) {
             System.out.println();
+        }
     }
 }
--- Lession/src/lession_2/OperatorLearning.java
+++ Lession/src/lession_2/OperatorLearning.java
@@ -2,6 +2,30 @@ package lession_2;
 
 public class OperatorLearning {
 
-    double myFloatNum = 10/3D;
+    public static void main(String[] args) {
+        int myNumber1 = 1;
+        int myMumber2 = 2;
 
+        // Left operand an right oerand
+        int total = myNumber1 + myMumber2;
+        // in xong nhảy xuống dòng mới thì: pritln
+        // in xong ở nguyên dòng cũ: print
+        System.out.println("Total: " + total);
+
+        // Print with format:
+        // %d: số nguyên - byte, short, interger, long;
+        // %f: số thực - float, double;
+        // %b: boolean - true/false;
+        // %s: charater, string...;
+        System.out.printf("Total: %d\n", total);
+
+        //Chia lấy phần nguyên
+        System.out.printf("\tPhep chia %d cho %d, co phan nguyen la %d\n", 5, 2, 5 / 2);
+
+        // Chia lấy phần dư
+        System.out.printf("\tPhep chia %d cho %d, co phan du la %d\n", 5, 2, 5 % 2);
+
+        // Mún thụt vào 1 ô : \t ở cuối
+        // Mún xuống dòng: \n ở cuối
+    }
 }
